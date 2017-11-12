f57bc604cb5278ed2f8bbd8604d8160ee6eb6941
JSpeed8th <jspeed8th@gmail.com>
2017-11-12 20:30:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ var ageIsValid = Number.isInteger(currentAge);
 
 var randomNumber = Math.floor(Math.random() * 20);
 
-var randomInteger = Math.ceil(Math.random() * 20);
+var randomInteger = Math.ceil(randomNumber * 20);
